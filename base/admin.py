9180eb0b8f8d74d3f7ba5e0c5898c5b71ccad074
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-27 13:39:30 +0500

    init: app

--- base/admin.py
+++ base/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import Category, Blog
 
 # Register your models here.
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'publish_time', 'status']
-    list_filter = ['status', 'created_time', 'publish_time']
-    prepopulated_fields = {"slug":("title",)}
-    date_hierarchy = 'publish_time'
-    search_fields = ['title', 'body']
-    ordering = ['status', 'publish_time']
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
-
-# admin.site.register(Category, CategoryAdmin)
--- base/views.py
+++ base/views.py
@@ -1,29 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .models import Blog
 
 # Create your views here.
-
-class HomePageView(TemplateView):
-    model = Blog
-    template_name = 'pages/home.html'
-
-
-
-class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
-
-class PricePageView(TemplateView):
-    template_name = 'pages/price.html'
-
-class ServicePageView(TemplateView):
-    template_name = 'pages/service.html'
-
-class ShopPageView(TemplateView):
-    template_name = 'pages/shop.html'
-
-class BlogPageView(TemplateView):
-    template_name = 'pages/blog.html'
-
-class ContactPageView(TemplateView):
-    template_name = 'pages/contact.html'
