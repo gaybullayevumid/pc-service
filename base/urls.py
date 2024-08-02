9110da4dd844b4d70f37b0b3b0805a43066bfd59
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-08-02 12:53:56 +0500

    feat: connect views to urls

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('pricing/', PricingPageView.as_view(), name='pricing'),
+    path('service/', ServicePageView.as_view(), name='service'),
+    path('shop/', ShopPageView.as_view(), name='shop'),
+    path('blog/', BlogPageView.as_view(), name='blog'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
