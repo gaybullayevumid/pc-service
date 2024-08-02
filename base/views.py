df57e5f64b98a44fdc2e9152a7b35d5b335bf2c1
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-08-02 12:50:14 +0500

    feat: create pageview

--- base/views.py
+++ base/views.py
@@ -5,3 +5,21 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+class PricingPageView(TemplateView):
+    template_name = "pages/price.html"
+
+class ServicePageView(TemplateView):
+    template_name = "pages/service.html"
+
+class ShopPageView(TemplateView):
+    template_name = "pages/shop.html"
+
+class BlogPageView(TemplateView):
+    template_name = "pages/blog.html"
+
+class ContactPageView(TemplateView):
+    template_name = "pages/contact.html"
